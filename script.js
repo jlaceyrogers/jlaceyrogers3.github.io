169fda8d9e4e448dc00953f3ed20dcd6b1e3e7ee
Jack Lacey-Rogers <jacklaceyrogers6@gmail.com>
2023-04-04 11:46:26 -0400

    added logic for present, past and future classes

--- script.js
+++ script.js
@@ -16,23 +16,46 @@ $(function() {
   }
 
   saveBtn.on("click", saveBtnClick);
+
+  //this is the current time
+  var currentHour = dayjs().hour();
+
+
+  //loops through each time block
+  $(".time-block").each(function() {
+
+    //this is the time block hour
+    var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+
+    //aplying the past, present, and future classes
+    if (timeBlockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (timeBlockHour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("Future");
+    }
+
+  });
 });
 
 
 
+// Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. 
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
+
+
+
+
+
+
+
+
+  //HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
+
+
+
   
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
